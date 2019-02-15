5efde78f998d8ca10d8dc3c224eabff2920a1b41
heecheol <msn9975@gmail.com>
2019-02-16 00:01:24 +0900

    최근 포스트 업데이트

--- client/src/spa/components/card/CurrentPostsDef.js
+++ client/src/spa/components/card/CurrentPostsDef.js
@@ -1,25 +1,25 @@
 export const postgroup1 = [
   {
-    title: 'Angular 라우팅과 네비게이션',
-    robot: '라우팅과 네비게이션에 들어가기전에 Angular, React, Vue 와 같은것들이 왜 생겨났는지에 대해 알아 볼 필요가 있습니다. 현재 모바일로 인터넷을 접속하는 일이 많아지고, 웹이 더이상 \'문서\'에 국한되지않고 마치 네이티브 앱처럼 하나의 애플리케이션으로 자리잡고 있습니다. 사용자와의 인터랙션들이 많아지고 사용자들을 웹에서도 네이티브 앱처럼 반응성, 트래픽 감소, 속도 등을 느끼고 싶었을 겁니다.',
-    route: 'angular/posts/17',
+    title: '구조적 디렉티브 활용',
+    robot: '*ngFor 문법은 아래의 형태로 변환됩니다. 눈여겨볼 점은 다음과 같습니다. ngFor 디렉티브 [ngForOf] 어트리뷰트의 list 속성 바인딩 ngFor 와 ngForOf 가 selector 에 정의되어있고, ngForOf 에 데이터바인딩이 됩니다. 그리고 ngForOf 는 NgIterable<T> 타입으로 정의되고 있습니다.',
+    route: 'angular/posts/22',
   },
   {
-    title: 'Angular 모듈',
-    robot: '앵귤러의 모듈(NgModule) 은 관련이 있는 구성요소(컴포넌트, 디렉티브, 파이프, 서비스 등)를 하나의 단위로 묶는 메커니즘을 말합니다. 이 장에서는 모듈에 대해 다뤄보겠습니다.',
-    route: 'angular/posts/16',
+    title: 'ElementRef, ViewRef, ComponentRef, ViewContainerRef',
+    robot: '역할 특정 엘리먼트를 기반으로 작동해야 하는 어떤 기능에 대해 대상 엘리먼트를 지정해주는 역할을 합니다. 특징 다양한 환경(플랫폼)에서 실행될 수 있는것을 염두에 둔 추상화된 객체, 플랫폼에 따라 nativeElement 는 다른 객체가 됩니다. 생성자에 주입되는 ElementRef 는 루트 엘리먼트 다른 플랫폼에서도 실행될 가능성을 염두에 둔 부분이기 때문에 Renderer2 의 메서드를 이용하여 처리하는것이 좋습니다.',
+    route: 'angular/posts/21',
   },
 ];
 
 export const postgroup2 = [
   {
-    title: 'Angular 폼과 유효성 검증',
-    robot: '웹 애플리케이션에서 폼은 사용자의 입력을 받는 인터페이스 입니다. HTML 표준 입력폼을 사용해도되지만 단점과 한계(submit 누르면 페이지전환, 세밀한 유효성검사의 어려움 등)가 있습니다. 그리고 Angular 폼을 사용하여도 애플리케이션이 복잡해지면 테스트도 힘들어집니다.',
-    route: 'angular/posts/15',
+    title: '<ng-template> 과 동적 뷰',
+    robot: '먼저, <template> 의 태그를 알아보자면 미리 정의해 둔 HTML 을 필요한 시점에 동적으로 인스턴스화해 출력을 삽입할 수 있는 표준적인 방법 입니다. 즉, DOM 트리에는 존재하지만 인스턴스화를 통해 화면에 출력이 된다는 것입니다.',
+    route: 'angular/posts/20',
   },
   {
-    title: 'Angular HTTP 통신',
-    robot: '대부분의 웹 애플리케이션에서 HTTP 통신은 필수입니다. HttpClient 클래스를 통해 HTTP 요청 API와 인터셉터를 제공합니다. 이 섹션에서는 http 통신에 대해서 다뤄보겠습니다.',
-    route: 'angular/posts/14',
+    title: '변경감지 전략',
+    robot: 'OnChanges 는 @Input 멤버변수의 레퍼런스가 변경될 때만 호출이 된다고 하였는데, 데이터의 레퍼런스가 변경되지 않았음에도 어쨌든 컴포넌트의 데이터가 변경되었는지 확인을 하는 작업을 수행합니다.',
+    route: 'angular/posts/19',
   },
 ];
